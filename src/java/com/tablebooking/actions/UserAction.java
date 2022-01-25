56120147ab74185981422351a7dcc7129bf47586
Apurba Bhattacharjee <apurba1603@gmail.com>
2022-01-25 22:07:51 +0530

    Added User Action and UserServices

---
+++ src/java/com/tablebooking/actions/UserAction.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Struts2/ActionSupport.java to edit this template
+ */
+package com.tablebooking.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author Apu
+ */
+public class UserAction extends ActionSupport {
+    
+    public UserAction() {
+    }
+    
+    public String execute() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
